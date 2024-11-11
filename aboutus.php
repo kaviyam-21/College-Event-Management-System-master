895e3896716485461ea9b7ce41bac22b7db1a387
kaviyam-21 <kaviyam005@gmail.com>
2024-11-11 12:23:24 +0530

    first commit

---
+++ aboutus.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>cems</title>
+<?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+</head>
+<style>
+
+/* Large rounded green border */
+hr.blueline {
+  border: 1px solid #90a4ae;
+  border-radius: 100px;
+}
+
+#bj
+{
+  font-size: 22px;
+}
+</style>
+
+
+
+  <?php require 'utils/header.php'; ?>
+  <hr class="blueline">
+  <div class="col-md-12">
+  
+<h1>About Us</h1>
+
+<p> Our college Mission is to impart quality technical education and higher moral ethics<br>
+  associated with skilled training to suit the modern day technology with innovative concepts,so as to learn to lead the future<br>
+  with full confidence
+                  .</p><br><br>
+
+</div>
+<hr class="blueline">
+</body>
+
+ <?php require 'utils/footer.php'; ?>
+
+</html>
---
+++ utils/footer.php
@@ -0,0 +1,8 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <h4> Questions? Email us anytime </h4> 
+    <a href="http://localhost/college-Event-Management-System-master/contact.php">Contact Us</a>
+
+    </div>
+</footer>
